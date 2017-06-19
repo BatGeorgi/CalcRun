8b078d873ec7db71ba2fe7c10912a6fbb2ad2a4c
Georgi Boyvalenkov <g.boyvalenkov@prosyst.bg>
2017-06-19 14:03:10 +0300

    Initial import

---
+++ src/xrun/CalcDist.java
@@ -0,0 +1,338 @@
+package xrun;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class CalcDist {
+  
+  private static final double COEF = 5.0 / 18.0; // km/h to m/s
+  
+  private static final double[] BOUNDS = new double[] {
+      0.0, 6.0 * COEF, 7.0 * COEF, 8.0 * COEF, 9.0 * COEF, 10.0 * COEF, 11.0 * COEF, 12.0 * COEF
+  }; // m/s
+  
+  private static final double DEFAULT_RUNNING = 9.0; // km/h
+  private static final double DEFAULT_INTERVAL = 100; // meters
+  
+  private File file;
+  private double minRunningSpeedKmh;
+  private double minRunningSpeed; // m/s
+  private double interval; // meters
+  private double splitM; // meters
+  
+  private String userFriendlyName;
+  private double[] histDist = new double[BOUNDS.length];
+  private double[] histElePos = new double[BOUNDS.length];
+  private double[] histEleNeg = new double[BOUNDS.length];
+  private double[] histTime = new double[BOUNDS.length];
+  private List<Double> splitTimes = new ArrayList<Double> ();
+  private List<Double> splitEle = new ArrayList<Double>();
+  private double splitRem;
+  
+  private CalcDist(File file, double minRunningSpeed, double interval, double splitM) {
+    this.file = file;
+    minRunningSpeedKmh = minRunningSpeed;
+    this.minRunningSpeed = (minRunningSpeed * 5.0) / 18.0; // convert to m/s
+    this.interval = interval;
+    this.splitM = splitM;
+  }
+  
+  private Node getDirectChild(Node parent, String name) {
+      for(Node child = parent.getFirstChild(); child != null; child = child.getNextSibling()) {
+          if(name.equals(child.getNodeName())) {
+            return child;
+          }
+      }
+      return null;
+  }
+  
+  private double distance(double lat1, double lat2, double lon1,
+      double lon2) {
+    final int R = 6371; // Radius of the earth
+    double latDistance = Math.toRadians(lat2 - lat1);
+    double lonDistance = Math.toRadians(lon2 - lon1);
+    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+        + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c * 1000.0; // convert to meters
+  }
+  
+  private String format(long seconds) {
+    int hours = (int) (seconds / 3600);
+    int minutes = (int) ((seconds % 3600) / 60);
+    seconds = (int) seconds % 60;
+    StringBuffer sb = new StringBuffer();
+    if (hours > 0) {
+      sb.append(hours + "h, ");
+    }
+    if (minutes > 0) {
+      sb.append(minutes + "m, ");
+    }
+    sb.append(seconds + "s");
+    return sb.toString();
+  }
+  
+  private void hist(double speed, double dist, double time, double ele) {
+    for (int i = BOUNDS.length - 1; i >= 0; --i) {
+      if (speed >= BOUNDS[i]) {
+        histDist[i] += dist;
+        histTime[i] += time;
+        if (ele > 0) {
+          histElePos[i] += ele;
+        } else {
+          histEleNeg[i] -= ele;
+        }
+        break;
+      }
+    }
+  }
+  
+  private void process(StringBuffer sb) throws Exception {
+    System.out.println("Processing " + file);
+    DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    InputStream is = null;
+    try {
+      is = new FileInputStream(file);
+      Document document = builder.parse(is);
+      String name = document.getElementsByTagName("name").item(0).getTextContent();
+      String timeStart = null;
+      NodeList list = document.getElementsByTagName("trkpt");
+      long lastTime = Long.MIN_VALUE;
+      double[] prev = null;
+      double currentDist = 0.0;
+      double currentTime = 0.0;
+      double distRunning = 0.0;
+      double timeRunning = 0.0;
+      double distTotal = 0.0;
+      double timeTotal = 0.0;
+      double timeRest = 0.0;
+      double currentDistSplits = 0.0;
+      double currentTimeSplits = 0.0;
+      double currentEleSplits = 0.0;
+      
+      double currentEle = 0.0;
+      double eleRunningPos = 0.0;
+      double eleRunningNeg = 0.0;
+      double eleTotalPos = 0.0;
+      double eleTotalNeg = 0.0;
+      
+      for (int i = 0; i < list.getLength(); ++i) {
+        Node node = list.item(i);
+        String latS = node.getAttributes().getNamedItem("lat").getNodeValue();
+        String lonS = node.getAttributes().getNamedItem("lon").getNodeValue();
+        String eleS = getDirectChild(node, "ele").getTextContent();
+        double lat = Double.parseDouble(latS);
+        double lon = Double.parseDouble(lonS);
+        double ele = Double.parseDouble(eleS);
+        String timeS = getDirectChild(node, "time").getTextContent();
+        if (i == 0) {
+          timeStart = timeS.substring(0, timeS.indexOf('T'));
+        }
+        StringTokenizer st = new StringTokenizer(timeS, "-T:.Z", false);
+        Calendar cal = new GregorianCalendar();
+        cal.set(Calendar.YEAR, Integer.parseInt(st.nextToken()));
+        cal.set(Calendar.MONTH, Integer.parseInt(st.nextToken()));
+        cal.set(Calendar.DATE, Integer.parseInt(st.nextToken()));
+        cal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(st.nextToken()));
+        cal.set(Calendar.MINUTE, Integer.parseInt(st.nextToken()));
+        cal.set(Calendar.SECOND, Integer.parseInt(st.nextToken()));
+        if (st.hasMoreTokens()) {
+          cal.set(Calendar.MILLISECOND, Integer.parseInt(st.nextToken()));
+        }
+        if (i > 0) {
+          double tempDist = distance(prev[0], lat, prev[1], lon);
+          currentDist += tempDist;
+          currentDistSplits += tempDist;
+          double timeDiff = (cal.getTimeInMillis() - lastTime) / 1000.0;
+          currentTime += timeDiff;
+          currentTimeSplits += timeDiff;
+          currentEle += ele - prev[2];
+          currentEleSplits += ele - prev[2];
+          if (currentDist / currentTime < 0.5) {
+            timeRest += timeDiff;
+          }
+          boolean lastOne = i == list.getLength() - 1;
+          if (currentDist >= interval || lastOne) {
+            double speed = currentDist / currentTime;
+            hist(speed, currentDist, currentTime, currentEle);
+            if (speed >= minRunningSpeed) {
+              distRunning += currentDist;
+              timeRunning += currentTime;
+              if (currentEle > 0) {
+                eleRunningPos += currentEle;
+              } else {
+                eleRunningNeg -= currentEle;
+              }
+            }
+            distTotal += currentDist;
+            timeTotal += currentTime;
+            if (currentEle > 0) {
+              eleTotalPos += currentEle;
+            } else {
+              eleTotalNeg -= currentEle;
+            }
+            currentDist = 0.0;
+            currentTime = 0.0;
+            currentEle = 0.0;
+          }
+          if (currentDistSplits >= splitM) {
+            double coef = splitM / currentDistSplits;
+            double speed = currentDistSplits / currentTimeSplits;
+            double ctime = splitM / speed;
+            splitTimes.add(ctime);
+            splitEle.add(currentEleSplits * coef);
+            currentDistSplits -= splitM;
+            currentTimeSplits -= ctime;
+            currentEleSplits = currentEle * coef;
+          } else if (lastOne) {
+            splitRem = currentDistSplits;
+            splitTimes.add(currentTimeSplits);
+            splitEle.add(currentEleSplits);
+          }
+        }
+        prev = new double[] {lat, lon, ele};
+        lastTime = cal.getTimeInMillis();
+      }
+      sb.append("# Generated by " + file.getName() + "\r\n");
+      sb.append(name + "\r\n");
+      sb.append(timeStart + "\r\n");
+      String distKm = String.format("%.3f", (distTotal / 1000.0));
+      sb.append("Total distance is " + distKm + " km\r\n");
+      sb.append("Total running distance is " + String.format("%.3f", (distRunning / 1000.0)) + " km\r\n");
+      sb.append("Running(>=" + minRunningSpeedKmh + " km/h) in " + String.format("%.3f", (distRunning / distTotal) * 100.0) + "% of the path\r\n");
+      sb.append("Running time " + format((long) timeRunning) + "\r\n");
+      sb.append("Total time " + format((long) timeTotal) + "\r\n");
+      sb.append("Rest time " + format((long) timeRest) + "\r\n");
+      sb.append("Average speed: " + String.format("%.3f km/h", (distTotal / timeTotal) / COEF) + "\r\n");
+      sb.append("Elevation running: " + String.format("+%dm, -%dm", (long) eleRunningPos, (long) eleRunningNeg) + "\r\n");
+      sb.append("Elevation total: " + String.format("+%dm, -%dm", (long) eleTotalPos, (long) eleTotalNeg) + "\r\n");
+      userFriendlyName = "Track_" + timeStart;
+      userFriendlyName = userFriendlyName.replaceAll(" ", "") + "_" + distKm + "km";
+    } finally {
+      try {
+        if (is != null) {
+          is.close();
+        }
+      } catch (IOException ignore) {
+      }
+    }
+  }
+
+  public static void xmain(String[] args) throws Exception {
+    if (args.length == 0) {
+      throw new IllegalArgumentException("Input file not specified");
+    }
+    File file = new File(args[0]);
+    if (!file.isFile()) {
+      throw new IllegalArgumentException("Input file not valid");
+    }
+    double minR = DEFAULT_RUNNING;
+    double intrv = DEFAULT_INTERVAL;
+    if (args.length > 1) {
+      try {
+        minR = new Double(args[1]).doubleValue();
+      } catch (Exception ignore) {
+      }
+    }
+    if (args.length > 2) {
+      try {
+        intrv = new Double(args[2]).doubleValue();
+      } catch (Exception ignore) {
+      }
+    }
+    StringBuffer sb = new StringBuffer();
+    new CalcDist(file, minR, intrv, 1000).process(sb);
+    System.out.println(sb);
+  }
+  
+  static String run(String fileName, String minSpeed, String intR, String splitS) throws Exception {
+    if (fileName == null || fileName.length() == 0) {
+      throw new IllegalArgumentException("Input file not specified");
+    }
+    File file = new File(fileName);
+    if (!file.isFile()) {
+      throw new IllegalArgumentException("Input file not valid");
+    }
+    double minR = DEFAULT_RUNNING;
+    double intrv = DEFAULT_INTERVAL;
+    try {
+      minR = new Double(minSpeed).doubleValue();
+    } catch (Exception ignore) {
+    }
+    try {
+      intrv = new Double(intR).doubleValue();
+    } catch (Exception ignore) {
+    }
+    double splitM = 1000.0;
+    try {
+      splitM = new Double(splitS).doubleValue();
+    } catch (Exception ignore) {
+    }
+    StringBuffer sb = new StringBuffer();
+    CalcDist cd = new CalcDist(file, minR, intrv, splitM * 1000.0);
+    cd.process(sb);
+    if (cd.userFriendlyName != null) {
+      BufferedWriter writer = null;
+      File target = new File(file.getParent(), cd.userFriendlyName.replace(',', '.') + "_report.txt");
+      try {
+        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target), "UTF-8"));
+        writer.write(sb.toString());
+        writer.newLine();
+        writer.write("Speed distribution");
+        writer.newLine();
+        for (int i = 0; i < BOUNDS.length; ++i) {
+          writer.write(String.valueOf(Math.round(BOUNDS[i] / COEF)) +
+              (i < BOUNDS.length - 1 ? "-" + String.valueOf(Math.round(BOUNDS[i + 1] / COEF)) + ": "
+              : "+: "));
+          writer.write(String.format("%.3fkm", cd.histDist[i] / 1000.0) + " for " + cd.format((long) cd.histTime[i]));
+          writer.write(", elevation: " + String.format("+%dm, -%dm", (long) cd.histElePos[i], (long) cd.histEleNeg[i]));
+          writer.newLine();
+        }
+        writer.newLine();
+        writer.write("Splits");
+        writer.newLine();
+        double tot = 0.0;
+        for (int i = 0; i < cd.splitTimes.size(); ++i) {
+          if (i < cd.splitTimes.size() - 1 || cd.splitRem < 1e-6) {
+            tot += cd.splitM / 1000.0;
+            writer.write(String.format("%.3fkm: ", tot));
+          } else {
+            writer.write(String.format("%.3fkm: ", tot + cd.splitRem / 1000.0));
+          }
+          writer.write(cd.format((long) cd.splitTimes.get(i).doubleValue()));
+          double ele = cd.splitEle.get(i);
+          writer.write(", elevation: " + (ele > 0 ? "+" : "") + (long) ele + "m");
+          writer.newLine();
+        }
+        writer.flush();
+      } finally {
+        try {
+          if (writer != null) {
+            writer.close();
+          }
+        } catch (IOException ignore) {
+        }
+      }
+    }
+    return sb.toString();
+  }
+
+}
