d0b6b2bfc8ec2b05c302d531d2dfe0419836eb8f
Georgi Boyvalenkov <g.boyvalenkov@prosyst.bg>
2017-06-30 16:13:13 +0300

    1.1 release

---
+++ run_rescan.sh
@@ -0,0 +1,1 @@
+java -cp bin:jetty-all-9.4.6.v20170531-uber.jar:json.jar: xrun.RunCalcUtils C:/GBV/Tracks
--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,1 @@
-#TODO change the last argument to the proper tracks base
 java -cp bin:jetty-all-9.4.6.v20170531-uber.jar:json.jar: xrun.CalcDistServer 3865 C:/GBV/Tracks
