c67ea571da972ade615001f9b945e6c9d96b8b9f
BatGeorgi <7sparkle77@gmail.com>
2017-07-02 18:54:43 +0300

    scripts modified

--- run_rescan.sh
+++ run_rescan.sh
@@ -1,1 +1,1 @@
-java -cp bin:jetty-all-9.4.6.v20170531-uber.jar:json.jar: xrun.RunCalcUtils C:/GBV/Tracks
+java -cp bin:jetty-all-9.4.6.v20170531-uber.jar:json.jar xrun.RunCalcUtils /home/pi/Tracks
--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-java -cp bin:jetty-all-9.4.6.v20170531-uber.jar:json.jar: xrun.CalcDistServer 3865 C:/GBV/Tracks
+java -cp bin:jetty-all-9.4.6.v20170531-uber.jar:json.jar xrun.CalcDistServer 7272 /home/pi/Tracks
