a9611bd854077def4237fca0b432eddc894bf4bf
Georgi Boyvalenkov <g.boyvalenkov@prosyst.bg>
2017-08-08 18:05:41 +0300

    new features & styles

--- src/xrun/CalcDistServer.java
+++ src/xrun/CalcDistServer.java
@@ -179,6 +179,88 @@ class CalcDistHandler extends AbstractHandler {
     return false;
   }
   
+  private void checkAch(JSONObject achObj, JSONObject activity) {
+    double currentTime = activity.getDouble("timeTotalRaw");
+    double fkt = Double.MAX_VALUE;
+    if (achObj.has("raw")) {
+      fkt = achObj.optDouble("raw");
+    }
+    if (currentTime < fkt) {
+      achObj.put("raw", currentTime);
+      achObj.put("ach", activity.get("timeTotal"));
+      achObj.put("when", activity.get("date"));
+    }
+  }
+  
+  private JSONObject getBest() {
+    JSONObject result = new JSONObject();
+    JSONObject longestRun = new JSONObject();
+    JSONObject fastestRun = new JSONObject();
+    JSONObject maxAscent = new JSONObject();
+    JSONObject fastest1K = new JSONObject();
+    JSONObject fastest2K5 = new JSONObject();
+    JSONObject fastest5K = new JSONObject();
+    JSONObject fastest10K = new JSONObject();
+    JSONObject fastest21K = new JSONObject();
+    JSONObject fastest30K = new JSONObject();
+    JSONObject fastest42K = new JSONObject();
+    double maxDist = 0.0;
+    double maxSpeed = 0.0;
+    long maxEle = 0;
+    for (JSONObject activity : cache.values()) {
+      double dist = Double.parseDouble(activity.getString("dist"));
+      double speed = Double.parseDouble(activity.getString("avgSpeed"));
+      long ele = activity.getLong("eleTotalPos");
+      if (dist > maxDist) {
+        maxDist = dist;
+        longestRun.put("ach", dist + " km");
+        longestRun.put("when", activity.get("date"));
+      }
+      if (speed > maxSpeed) {
+        maxSpeed = speed;
+        fastestRun.put("ach", speed + " km/h");
+        fastestRun.put("when", activity.get("date"));
+      }
+      if (ele > maxEle) {
+        maxEle = ele;
+        maxAscent.put("ach", ele + " m");
+        maxAscent.put("when", activity.get("date"));
+      }
+      if (dist >= 0.99 && dist < 1.1) {
+        checkAch(fastest1K, activity);
+      }
+      if (dist >= 2.49 && dist < 2.55) {
+        checkAch(fastest2K5, activity);
+      }
+      if (dist >= 4.99 && dist < 5.2) {
+        checkAch(fastest5K, activity);
+      }
+      if (dist >= 9.98 && dist < 10.3) {
+        checkAch(fastest10K, activity);
+      }
+      if (dist >= 21 && dist < 21.8) {
+        checkAch(fastest21K, activity);
+      }
+      if (dist >= 30 && dist < 31) {
+        checkAch(fastest30K, activity);
+      }
+      if (dist >= 42 && dist < 43.5) {
+        checkAch(fastest42K, activity);
+      }
+    }
+    result.put("longest", longestRun);
+    result.put("fastest", fastestRun);
+    result.put("maxAsc", maxAscent);
+    result.put("1K", fastest1K);
+    result.put("2K5", fastest2K5);
+    result.put("5K", fastest5K);
+    result.put("10K", fastest10K);
+    result.put("21K", fastest21K);
+    result.put("30K", fastest30K);
+    result.put("42K", fastest42K);
+    return result;
+  }
+  
   private JSONObject filter(String nameFilter, boolean run, boolean trail, boolean hike, boolean walk, boolean other, int records,
       Calendar startDate, Calendar endDate, int minDistance, int maxDistance) {
     JSONObject result = new JSONObject();
@@ -264,6 +346,13 @@ class CalcDistHandler extends AbstractHandler {
       HttpServletResponse response) throws IOException, ServletException {
     if (!"POST".equals(baseRequest.getMethod())) {
       return;
+    }
+    if ("/best".equalsIgnoreCase(target)) {
+      response.setContentType("application/json");
+      response.getWriter().println(getBest());
+      response.getWriter().flush();
+      response.setStatus(HttpServletResponse.SC_OK);
+      baseRequest.setHandled(true);
     }
     if ("/fetch".equalsIgnoreCase(target)) {
       boolean run = "true".equals(baseRequest.getHeader("run"));
