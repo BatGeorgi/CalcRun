84639d2fb96ba37d4c1de979c265888aa439d736
Georgi Boyvalenkov <g.boyvalenkov@prosyst.bg>
2018-07-12 14:02:07 +0300

    performance util v100 charts

--- src/xrun/tools/PerformanceUtil.java
+++ src/xrun/tools/PerformanceUtil.java
@@ -19,35 +19,83 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class PerformanceUtil {
   
+  /*
+    PERFORMANCE SUBSETS:
+     elite: below 10:00
+     very fast: 10:00 - 12:00
+     fast: 12:00 - 14:30
+     regular: 14:30 - 17:00
+     tourists: over 17:00  
+     all: completed race
+     
+     TARGETS:
+      fiction: 11:00
+      machine: 11:30
+      great: 12:00
+      optimistic: 12:30
+      realistic: 13:00
+      current: 13:40
+   */
+  
   private static final String RESULTS_DEF = "C:/GBV/Tracks/allparts.txt";
-  private static final double[] SPLITS_DEF = new double[] {32, 59, 75, 98.4};
-  private static final String TIME_MIN_DEF = "08:30:00";
-  private static final String TIME_MAX_DEF = "09:30:00";
-  private static final String TIME_TARGET_DEF = "13:40:00";
+  private static final Double[] SPLITS_DEF = new Double[] {32.0, 59.0, 75.0, 98.4};
+  private static final String TIME_MIN_DEF = "11:30:00";
+  private static final String TIME_MAX_DEF = "14:00:00";
+  private static final String TIME_TARGET_DEF = "13:00:00";
   
   private File results;
-  private double[] splits;
+  private Double[] splits;
   private long timeMin;
   private long timeMax;
   private long timeTarg;
 
   public static void main(String[] args) throws Exception {
-    File results = new File(args.length > 0 ? args[0] : RESULTS_DEF);
-    double[] splits = SPLITS_DEF;
-    // TODO parse splits from args if available
+    populateData();
+    /*File results = new File(args.length > 0 ? args[0] : RESULTS_DEF);
+    Double[] splits = SPLITS_DEF;
+    if (args.length > 1) {
+      List<Double> sp = new LinkedList<>();
+      String[] tokens = args[1].split("\\s+");
+      for (String token : tokens) {
+        sp.add(Double.parseDouble(token));
+      }
+      splits = new Double[sp.size()];
+      sp.toArray(splits);
+    }
     String timeMin = args.length > 2 ? args[2] : TIME_MIN_DEF;
     String timeMax = args.length > 3 ? args[3] : TIME_MAX_DEF;
     String timeTarg = args.length > 4 ? args[4] : TIME_TARGET_DEF;
-    System.out.println("Processing input " + results + ", splits = " + Arrays.asList(splits) +
-        " in range [" + timeMin + ", " + timeMax + "]");
-    new PerformanceUtil(results, splits, timeMin, timeMax, timeTarg).calc();
+    new PerformanceUtil(results, splits, timeMin, timeMax, timeTarg).calc();*/
   }
   
-  PerformanceUtil(File results, double[] splits, String timeMin, String timeMax, String timeTarg) {
+  private static void populateData() throws Exception {
+    String[] targets = new String[] {
+        "11:00:00",
+        "11:30:00",
+        "12:00:00",
+        "12:30:00",
+        "13:00:00",
+        "13:40:00",
+    };
+    for (String target : targets) {
+      new PerformanceUtil(new File(RESULTS_DEF), SPLITS_DEF, "00:00:00", "10:00:00", target).calc();
+      new PerformanceUtil(new File(RESULTS_DEF), SPLITS_DEF, "10:00:01", "12:00:00", target).calc();
+      new PerformanceUtil(new File(RESULTS_DEF), SPLITS_DEF, "12:00:01", "14:30:00", target).calc();
+      new PerformanceUtil(new File(RESULTS_DEF), SPLITS_DEF, "14:30:01", "17:00:00", target).calc();
+      new PerformanceUtil(new File(RESULTS_DEF), SPLITS_DEF, "17:00:01", "20:00:00", target).calc();
+      new PerformanceUtil(new File(RESULTS_DEF), SPLITS_DEF, "00:00:00", "20:00:00", target).calc();
+    }
+  }
+  
+  PerformanceUtil(File results, Double[] splits, String timeMin, String timeMax, String timeTarg) {
+    System.out.println("Processing input " + results + ", splits = " + Arrays.asList((Object[]) splits) +
+        " in range [" + timeMin + ", " + timeMax + "] for target time " + timeTarg);
     this.results = results;
     this.splits = splits;
     this.timeMin = getTimeInSeconds(timeMin);
